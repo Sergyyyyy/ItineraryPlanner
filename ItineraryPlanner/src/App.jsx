bd14d86e9624ee8bb5cd779e5e18f016b147f6d8
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-16 13:48:08 +0800

    Imported Form.jsx file, cleaned boiler code.

--- ItineraryPlanner/src/App.jsx
+++ ItineraryPlanner/src/App.jsx
@@ -1,10 +1,15 @@
+import Input from "./myComponents/Input";
+import Form from "./myComponents/Form";
 
 function App() {
   return (
     <>
-      
+      <div className="app-container">
+        <h3>Itinerary Planner</h3>
+        <Form />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

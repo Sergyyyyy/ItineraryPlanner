f0c5b751d0433e071a245082551fe44545158c59
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-16 19:29:57 +0800

    Integrated form design with current Input.jsx component, minor adjustment to code structure.

--- ItineraryPlanner/src/myComponents/Form.jsx
+++ ItineraryPlanner/src/myComponents/Form.jsx
@@ -4,7 +4,21 @@ const Form = () => {
   return (
     <>
       <div className="form-container">
-        <Input />
+        <div className="form-top-container">
+          <p>⁠♡Itinerary Planner⁠♡</p>
+        </div>
+
+        <div className="form-mid-container">
+          <div className="form-main-container">
+            <Input />
+            <div className="btn-container">
+              <button className="btn btn-clearEvent">Clear events -</button>
+              <button className="btn btn-addEvent">Add new event +</button>
+            </div>
+          </div>
+
+          <button className="btn btn-downloadEvent">Download</button>
+        </div>
       </div>
     </>
   );

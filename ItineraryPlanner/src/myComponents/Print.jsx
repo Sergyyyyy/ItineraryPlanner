668029d98035001d344e6693b1a0ccd59e5c14b1
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-18 01:29:05 +0800

    Added Print.jsx to handle main pdf storage + download capability.

---
+++ ItineraryPlanner/src/myComponents/Print.jsx
@@ -0,0 +1,13 @@
+
+const Print = ({setPage, events}) => {
+    return(
+        <>
+            {
+
+            }
+            {/* <button onClick={() => setPage("form")}>Back</button> */}
+        </>
+    )
+}
+
+export default Print;

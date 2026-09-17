69bc24e9cc461af380c6af46a97fe679ffb85ee5
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-18 01:27:38 +0800

    Transferred the states to App.jsx.

--- ItineraryPlanner/src/App.jsx
+++ ItineraryPlanner/src/App.jsx
@@ -1,11 +1,21 @@
 import Input from "./myComponents/Input";
 import Form from "./myComponents/Form";
+import Print from "./myComponents/Print";
+import { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState('form');
+  const [event, setEvent] = useState({
+    eventName: "",
+    eventTime: "",
+  });
+  const [events, setEvents] = useState([]);
+
   return (
     <>
       <div className="app-container">
-        <Form />
+        {page === 'form' && <Form setPage={setPage} event={event} setEvent={setEvent} events={events} setEvents={setEvents}/>}
+        {page === 'print' && <Print setPage={setPage}/>}
       </div>
     </>
   );

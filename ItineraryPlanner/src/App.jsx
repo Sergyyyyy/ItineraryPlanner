b8dd97e38f132e914e8976798440f42d02f0f0e3
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-18 18:21:51 +0800

    Added setEvents reference to Print.jsx

--- ItineraryPlanner/src/App.jsx
+++ ItineraryPlanner/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <div className="app-container">
         {page === 'form' && <Form setPage={setPage} event={event} setEvent={setEvent} events={events} setEvents={setEvents}/>}
-        {page === 'print' && <Print setPage={setPage} events={events}/>}
+        {page === 'print' && <Print setPage={setPage} events={events} setEvents={setEvents}/>}
       </div>
     </>
   );

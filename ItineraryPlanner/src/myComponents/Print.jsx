f6d1ce00a80f9f0f5107a731183be49642311616
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-18 17:45:12 +0800

    Added form structure, implemented map to iterate through all events.

--- ItineraryPlanner/src/myComponents/Print.jsx
+++ ItineraryPlanner/src/myComponents/Print.jsx
@@ -1,13 +1,25 @@
-
-const Print = ({setPage, events}) => {
-    return(
-        <>
-            {
-
-            }
-            {/* <button onClick={() => setPage("form")}>Back</button> */}
-        </>
-    )
-}
+const Print = ({ setPage, events }) => {
+  return (
+    <>
+      <div className="form-container">
+        <div className="form-top-container">
+          <p>⁠♡ Current Event List ⁠♡</p>
+        </div>
+        <div className="event-list-container">
+          {events.map((event, index) => {
+            return (
+              <p className="event-list-item">
+                <b>{index + 1}</b>. {event.eventName} - {event.eventTime}
+              </p>
+            );
+          })}
+        </div>
+        <button onClick={() => setPage("form")} className="btn btn-backEvent">
+          Back
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Print;

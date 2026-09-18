af61fcd07ff6efe3ba8bc283e45d6e87036c7fd6
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-09-19 04:20:01 +0800

    fix: restructure project for Netlify deployment

---
+++ src/myComponents/Form.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import Input from "./Input";
+
+const Form = ({setPage, event, setEvent, events, setEvents}) => {
+
+  const clearEvent = () => {
+    setEvent({
+      eventName: "",
+      eventTime: "",
+    });
+  };
+
+  const addEvent = () => {
+    setEvents([...events, event]);
+
+    clearEvent();
+  };
+
+  useEffect(() => {
+    console.log(events);
+  }, [events]);
+
+  return (
+    <>
+      <div className="form-container">
+        <div className="form-top-container">
+          <p>⁠♡  Itinerary Planner ⁠♡</p>
+        </div>
+
+        <div className="form-mid-container">
+          <div className="form-main-container">
+            <Input event={event} setEvent={setEvent} />
+            <div className="btn-container">
+              <button className="btn btn-clearEvent" onClick={clearEvent}>
+                Clear Input
+              </button>
+              <button className="btn btn-addEvent" onClick={addEvent}>
+                Add new Event
+              </button>
+            </div>
+          </div>
+
+          <button className="btn btn-viewEvent" onClick={() => setPage("print")}>View Events</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Form;
